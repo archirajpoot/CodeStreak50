8664c250bf3fba478f5ed61b23a074f90cb4a524
archirajpoot <archirajpoot21@gmail.com>
2025-10-10 16:21:38 +0530

    Day2

---
+++ Day2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int arr[100];
+    int n;
+    printf("enter the number of element of array");
+    scanf("%d",&n);
+    printf("enter the element of array\n");
+    for(int i=0;i<n;i++){
+      printf("arr[%d]:",i);
+      scanf("%d",&arr[i]);
+    }
+    printf("reverse of the array will be\n");
+    for(int i=0;i<n/2;i++){
+       int temp=arr[i];
+       arr[i]=arr[n-i-1];
+       arr[n-i-1]=temp;
+    }
+    for(int i=0;i<n;i++){
+      printf("arr[%d]:%d\n",i,arr[i]);
+    }
+    return 0;
+}
